c7da3b15add3c9024bbd5ba9e661a002e798d388
duthaegaux <wrrr000@mail.ru>
2025-06-20 22:21:05 +0300

    Now changing properties in settings.py, generating info.txt and code changes

--- parseJson.py
+++ parseJson.py
@@ -1,106 +1,49 @@
 import os
 import re
+import settings
+import time
 
-def change(string: str, dat: dict, re_pat: str, out):
-    if dat == 0: 
-        out.write(string)
+def change(line: str, data: list, re_pat: str, out):
+    if data == 0:
+        out.write(line)
         return
-    for el in dat.keys():
-        if f"\"{el}\":" in string:
-            out.write(string.replace(re.findall(re_pat, string)[0], str(dat[el])))
-            return
-    out.write(string)
+    for key in data.keys():
+        if f"\"{key}\":" in line:
+            num = re.findall(re_pat, line)
+            if len(num) == 1:
+                out.write(line.replace(num[0], str(data[key])))
+                return
+    out.write(line)
+
+def trace(weapon: str, cal: str, info: dict):
+    if cal in info:
+        info[cal].append(weapon)
+        return info
+    info[cal] = [weapon]
+    return info
+
+def outp(dct: dict):
+    info = open("info.txt", "w")
+    for key in dct.keys():
+        info.write(f"\n\n{key}\n\t")
+        c = 1
+        for element in dct[key]:
+            info.write(f"{element} ")
+            if c % 6 == 0:
+                info.write("\n\t")
+            c+=1
+    info.close()
+            
 
 jsons = os.listdir("./jsons")
-data = {
-    "602x41": {
-        "damage": 10.2,
-        "speed": 800,
-        "head_shot_multiplier": 1.5,
-        "armor_ignore": 0.45,
-        "life": 1.2,
-        "gravity": 0.0,
-        "knockback": 0.0
-    },
-    "762x39": {
-        "damage": 7.0,
-        "speed": 760,
-        "head_shot_multiplier": 2.0,
-        "armor_ignore": 0.3,
-        "life": 1.2,
-        "gravity": 0.0,
-        "knockback": 0.0
-    },
-    "545x39": {
-        "damage": 5.5,
-        "speed": 800,
-        "head_shot_multiplier": 1.2,
-        "armor_ignore": 0.3,
-        "life": 1.2,
-        "gravity": 0.0,
-        "knockback": 0.0
-    },
-    "556x45": {
-        "damage": 6.5,
-        "speed": 900,
-        "head_shot_multiplier": 1.7,
-        "armor_ignore": 0.3,
-        "life": 1.2,
-        "gravity": 0.0,
-        "knockback": 0.0
-    },
-    "308": {
-        "damage": 17.0,
-        "speed": 840,
-        "head_shot_multiplier": 2.0,
-        "armor_ignore": 0.3,
-        "life": 1.2,
-        "gravity": 0.0,
-        "knockback": 0.0
-    },
-    "9mm": {
-        "damage": 4.2,
-        "speed": 365,
-        "head_shot_multiplier": 1.2,
-        "armor_ignore": 0.1,
-        "life": 1.2,
-        "gravity": 0.0,
-        "knockback": 0.0
-    },
-    "50beowulf": {},
-    "300blk": {},
-    "57x28": {},
-    "300winmag": {},
-    "223remington": {},
-    "50bmg":{},
-    "can_blanks":{},
-    "556x30": {},
-    "12g_db": {},
-    "12g": {},
-    "762x54": {},
-    "12g_fl": {},
-    "75fk": {},
-    "40mm": {},
-    "338": {},
-    "45acp": {},
-    "416barrett": {},
-    "30_06": {},
-    "nails": {},
-    "bannana": {},
-    "65creedmoor": {},
-    "50ae": {},
-    "laser": {}
-}
 
-grav, knock = 0.0, 0.0
-
-# reAmmo = r"[\d]+x[\d]+"
-# reXz = r":[\d]+[\w]+"
-# reWtf = r":[^,]+"
-# reComb = f":{reWtf}|:{reXz}|:{reAmmo}|:{reNum}"
+data = settings.data
+standart = settings.standart
 
 reNum = r"[\d]+[.]{0,1}[\d]*"
 reAbsolute = r":[^,^\"^ ]+"
+
+info = dict()
 
 for json in jsons:
     file = open(f"./jsons/{json}", "r")
@@ -112,12 +55,23 @@ for json in jsons:
     dat = 0
     for line in lines:
         if "ammo" in line and dat == 0:
-            print(re.findall(reAbsolute, line)[0][1::])
-            dat  = data[re.findall(reAbsolute, line)[0][1::]]
+            cal = re.findall(reAbsolute, line)[0][1::]
+            print(cal)
+
+            if cal not in data:
+                data[cal] = dict()
+                print(f"\t{cal} not in settings! Adding it to avoid error")
+            dat = data[cal] | standart
+
+            info = trace(json.replace(".json", ""), cal, info)
+            
             
         change(line, dat, reNum, output)
     
     file.close()
     output.close()
 
-print("Ready! Check output folder.")
+print("\nReady! Check output folder.")
+
+outp(info)
+print("\nCreated info.txt file with sort by caliber.")
---
+++ settings.py
@@ -0,0 +1,128 @@
+data = {
+    "602x41": {
+        "damage": 10.2,
+        "speed": 800,
+        "head_shot_multiplier": 1.5,
+        "armor_ignore": 0.45,
+    },
+    "762x39": {
+        "damage": 7.0,
+        "speed": 760,
+        "head_shot_multiplier": 2.0,
+        "armor_ignore": 0.3,
+    },
+    "545x39": {
+        "damage": 5.5,
+        "speed": 800,
+        "head_shot_multiplier": 1.2,
+        "armor_ignore": 0.3,
+    },
+    "556x45": {
+        "damage": 6.5,
+        "speed": 900,
+        "head_shot_multiplier": 1.7,
+        "armor_ignore": 0.3,
+    },
+    "308": {
+        "damage": 17.0,
+        "speed": 840,
+        "head_shot_multiplier": 2.0,
+        "armor_ignore": 0.3,
+    },
+    "9mm": {
+        "damage": 4.2,
+        "speed": 365,
+        "head_shot_multiplier": 1.2,
+        "armor_ignore": 0.1,
+    },
+    "50beowulf": {
+        "damage": 20.0,
+        "speed": 550,
+        "head_shot_multiplier": 1.5,
+        "armor_ignore": 0.4,
+    },
+    "300blk": {
+
+    },
+    "57x28": {
+
+    },
+    "300winmag": {
+
+    },
+    "223remington": {
+
+    },
+    "50bmg":{
+
+    },
+    "can_blanks":{
+
+    },
+    "556x30": {
+
+    },
+    "12g_db": {
+
+    },
+    "12g": {
+
+    },
+    "762x54": {
+
+    },
+    "12g_fl": {
+
+    },
+    "75fk": {
+
+    },
+    "40mm": {
+
+    },
+    "338": {
+
+    },
+    "45acp": {
+
+    },
+    "416barrett": {
+
+    },
+    "30_06": {
+
+    },
+    "nails": {
+
+    },
+    "bannana": {
+
+    },
+    "65creedmoor": {
+
+    },
+    "50ae": {
+
+    },
+    "laser": {
+
+    },
+    "357mag": {
+
+    },
+    "9x39": {
+
+    },
+    "792x57": {
+
+    },
+    "46x30": {
+        
+    }
+}
+
+standart = {
+    "life": 1.2,
+    "gravity": 0.0,
+    "knockback": 0.0
+}
